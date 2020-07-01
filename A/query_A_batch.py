1015bf042dd37435b09ab5ce55b7d5e38964a6c4
froggagul <froggagul@gmail.com>
2020-07-01 16:03:47 +0900

    query update

--- A/query_A_batch.py
+++ A/query_A_batch.py
@@ -1,22 +1,41 @@
 import sqlite3
 import time
-import timeit
 #db connect
 db = sqlite3.connect('DB_A.sqlite')
-
+cursor = db.cursor()
 sql = '''
-select age, infection_case, state, count(*)
-from (select patient_id, count(distinct city) as countCity
+with cte
+as (
+select age, infection_case, 
+    sum(CASE WHEN state = 'isolated' THEN counts ELSE 0 END) AS isolated, 
+    sum(CASE WHEN state = 'released' THEN counts ELSE 0 END) AS released,
+	sum(CASE WHEN state = 'deceased' THEN counts ELSE 0 END) AS deceased
+from (
+	select age, infection_case, state, count(*) as counts
+    from (select patient_id, count(distinct city) as countCity
 	  from PatientRoute
 	  GROUP BY patient_id
 	) as FilteredPatientRoute LEFT JOIN PatientInfo
-WHERE FilteredPatientRoute.patient_id = PatientInfo.patient_id and countCity > 1
-GROUP BY age, infection_case, state
+	WHERE FilteredPatientRoute.patient_id = PatientInfo.patient_id and countCity > 1
+	GROUP BY age, infection_case, state
+	)
+GROUP BY age, infection_case
+)
+select age, infection_case,
+ isolated*100.0/(isolated + released + deceased) as isolatedPercentage,
+ released*100.0/(isolated + released + deceased) as releasedPercentage,
+ deceased*100.0/(isolated + released + deceased) as deceasedPercentage
+from cte
 '''
 
-
-for row in db.execute(sql):
-    print(row)
+start = time.time()
+for i in range(1000):
+    cursor.execute(sql)
+    tuples = cursor.fetchall()
+    print(tuples)
+stop = time.time()
+avg = (stop-start) / 1000
+print(round(avg, 6), "seconds")
 
 db.commit()
 db.close()
