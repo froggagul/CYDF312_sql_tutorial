65447c47dd8c88cb33081738cc10724dba6d66f7
froggagul <froggagul@gmail.com>
2020-07-01 23:20:51 +0900

    수정사항 반영

--- A/4/query_A.py
+++ A/4/query_A.py
@@ -4,58 +4,41 @@ db = sqlite3.connect('DB_A.sqlite')
 cursor = db.cursor()
 
 sql = '''
-select age, infection_case, state, count(*)
-from (select patient_id, count(distinct city) as countCity
+with cte
+as (
+select age, infection_case, 
+    sum(CASE WHEN state = 'isolated' THEN counts ELSE 0 END) AS isolated, 
+    sum(CASE WHEN state = 'released' THEN counts ELSE 0 END) AS released,
+	sum(CASE WHEN state = 'deceased' THEN counts ELSE 0 END) AS deceased
+from (
+	select age, infection_case, state, count(*) as counts
+    from (select patient_id, count(distinct city) as countCity
 	  from PatientRoute
-	  GROUP BY patient_id
+	  GROUP BY 
+      having countCity > 1
 	) as FilteredPatientRoute LEFT JOIN PatientInfo
-WHERE FilteredPatientRoute.patient_id = PatientInfo.patient_id and countCity > 1
-GROUP BY age, infection_case, state
+	WHERE FilteredPatientRoute.patient_id = PatientInfo.patient_id
+	GROUP BY age, infection_case, state
+	)
+GROUP BY age, infection_case
+)
+select age, infection_case,
+ isolated*100.0/(isolated + released + deceased) as isolatedPercentage,
+ released*100.0/(isolated + released + deceased) as releasedPercentage,
+ deceased*100.0/(isolated + released + deceased) as deceasedPercentage
+from cte
 '''
 
 cursor.execute(sql)
 tuples = cursor.fetchall()
-ageDict = {
-    "0s": {},
-    "10s": {},
-    "20s": {},
-    "30s": {},
-    "40s": {},
-    "50s": {},
-    "60s": {},
-    "70s": {},
-    "80s": {},
-    "90s": {},
-    "100s": {},
-    "None": {},
-}
-#index: isolated 0 released 1 deceased 2
+
 for tuple in tuples:
     age = tuple[0]
     place = tuple[1]
-    state = tuple[2]
-    value = tuple[3]
-    if state == 'isolated':
-        index = 0
-    elif state == 'released':
-        index = 1
-    elif state == 'deceased':
-        index = 2
-    else:
-        pass
-    if place in ageDict[age]:
-        ageDict[age][place][index] += value
-    else:
-        ageDict[age][place] = [0, 0, 0]
-        ageDict[age][place][index] += value
-print(ageDict)
-ages = ageDict.keys()
-for age in ages:
-    places = ageDict[age].keys()
-    for place in places:
-        val = ageDict[age][place]
-        tot = val[0] + val[1] + val[2]
-        print(age, place, str(int(val[0] / tot * 100)) + '%', str(int(val[1] / tot * 100)) + '%', str(int(val[2] / tot * 100)) + '%')
+    isolatedPercentage = tuple[2]
+    releasedPercentage = tuple[3]
+    deceasedPercentage = tuple[4]
+    print(tuple[0] + ' ' + tuple[1] + ' ' + str(int(tuple[2])) + '% ' + str(int(tuple[3])) + '% ' + str(int(tuple[4])) + '%')
 
 cursor.close()
 db.commit()
